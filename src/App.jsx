96dec150bc36d447e26baec027aba6eddd0d4cf1
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-26 23:40:58 +0000

    8

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route , Routes } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home/> }/>
+        <Route path="/about" element={ <About /> }/>
+        <Route path="/signin" element={ <Signin /> }/>
+        <Route path="/signup" element={ <Signup /> }/>
+        <Route path="/projects" element={ <Projects /> }/>
+        <Route path="/dashboard" element={ <Dashboard /> }/>
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
